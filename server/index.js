1fd8c778e3f07732ce1304cd928c19f83d926922
Andrii Ratsilevych <andrii.racilewicz@gmail.com>
2023-07-11 19:12:45 +0200

    return to early server

--- server/index.js
+++ server/index.js
@@ -1,41 +1,63 @@
 import {ApolloServer} from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import express from 'express';
-import http from 'http';
-import pkg from 'body-parser';
+import {startStandaloneServer} from '@apollo/server/standalone';
 import {typeDefs} from './models/typeDefs.js';
 import {resolvers} from './resolvers/resolvers.js';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config({ path: '../.env'})
 const Db = process.env.ATLAS_URI;
 const PORT = process.env.PORT;
-const { json } = pkg;
 
 const db = await mongoose.connect(`${Db}`);
 console.info(db ? 'Connected to CookBook DataBase' : 'Not connected toCookBook DataBase' );
 
-const app = express();
-app.use(cors());
-const httpServer = http.createServer(app);
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+const server = new ApolloServer({typeDefs, resolvers})
+const { url } = await startStandaloneServer(server, {
+  listen: {
+      port: PORT,
+  },
 });
+console.info(`🚀 Server ready at ${url}`);
 
-await server.start();
+// import {ApolloServer} from '@apollo/server';
+// import { expressMiddleware } from '@apollo/server/express4';
+// import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+// import express from 'express';
+// import http from 'http';
+// import pkg from 'body-parser';
+// import {typeDefs} from './models/typeDefs.js';
+// import {resolvers} from './resolvers/resolvers.js';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// dotenv.config({ path: '../.env'})
+// const Db = process.env.ATLAS_URI;
+// const PORT = process.env.PORT;
+// const { json } = pkg;
 
-app.use(
-  cors({
-    origin: ['*', 'https://my-cookbook-one.vercel.app/', 'https://vercel.live/link/my-cookbook-server-qtiisk4t6-feulife.vercel.app?via=deployment-domains-list-commit', 'https://my-cookbook-server-1ux59kkwk-feulife.vercel.app', 'https://my-cookbook-server.vercel.app/', 'https://vercel.com/feulife/my-cookbook-one/58yqWdyNyjfAQQzDywV1Tzw7cSJL']    
-  }),
-  json(),
-  expressMiddleware(server)
-);
+// const db = await mongoose.connect(`${Db}`);
+// console.info(db ? 'Connected to CookBook DataBase' : 'Not connected toCookBook DataBase' );
 
-await new Promise ((resolve) => httpServer.listen({ port: PORT }, resolve))
+// const app = express();
+// app.use(cors());
+// const httpServer = http.createServer(app);
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+// });
 
-console.info(`🚀 Server ready at http://localhost:${PORT}`);
+// await server.start();
+
+// app.use(
+//   cors({
+//     origin: ['*', 'https://my-cookbook-one.vercel.app/', 'https://vercel.live/link/my-cookbook-server-qtiisk4t6-feulife.vercel.app?via=deployment-domains-list-commit', 'https://my-cookbook-server-1ux59kkwk-feulife.vercel.app', 'https://my-cookbook-server.vercel.app/', 'https://vercel.com/feulife/my-cookbook-one/58yqWdyNyjfAQQzDywV1Tzw7cSJL']    
+//   }),
+//   json(),
+//   expressMiddleware(server)
+// );
+
+// await new Promise ((resolve) => httpServer.listen({ port: PORT }, resolve))
+
+// console.info(`🚀 Server ready at http://localhost:${PORT}`);
+
